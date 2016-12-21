9bea88eb3553737c39ca1241db7b809f35d6384b
Yuri Jean Fabris <yuri.fabris@gmail.com>
2016-12-21 18:31:38 -0200

    Add header

--- app/imports/client/Components/Header/index.js
+++ app/imports/client/Components/Header/index.js
@@ -6,39 +6,61 @@
 // Modules
 import React from 'react'
 import {
-	AppBar,
+	FontIcon,
 	IconMenu,
 	IconButton,
 	MenuItem,
 } from 'material-ui'
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-
-const Menu = (props) => (
-  <IconMenu
-    {...props}
-    iconButtonElement={
-      <IconButton><MoreVertIcon /></IconButton>
-    }
-    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-  >
-    <MenuItem primaryText="Refresh" />
-    <MenuItem primaryText="Help" />
-    <MenuItem primaryText="Sign out" />
-  </IconMenu>
-);
+import EventSeatIcon from 'material-ui/svg-icons/action/event-seat'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import {
+	white,
+	cyan400,
+	grey100,
+	grey800,
+} from 'material-ui/styles/colors';
 
 export default function Header(props) {
 	return (
 	  <div>
-	    <AppBar
-	      title="Emergence"
-	      iconElementRight={<Menu />}
+	    <div
 	      style={{
-	      	paddingLeft: 150,
-	      	paddingRight: 150,
+	      	padding: '7px 150px',
+	      	backgroundColor: white,
+	      	borderBottom: '1px solid #c5cfdc',
+	      	display: 'flex',
+	      	alignItems: 'center',
 	      }}
-	    />
+	    >
+	    	<EventSeatIcon
+	    		color={cyan400}
+	    		style={{
+	    			marginRight: 20,
+	    			height: 40,
+	    			width: 40,
+	    		}}
+	    	/>
+	    	
+	    	<input
+	    		type="text"
+	    		placeholder="Search for"
+	    		style={{
+	    			padding: 15,
+	    			border: 'none',
+	    			backgroundColor: grey100,
+	    			flexGrow: 100000,
+	    		}}
+	    	/>
+
+	    	<p
+	    		style={{
+	    			marginLeft: 50,
+	    			color: grey800,
+	    		}}
+	    	>
+	    		Login
+	    	</p>
+	    </div>
 	  </div>
 	)
 }
