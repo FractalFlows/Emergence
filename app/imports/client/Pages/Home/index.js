490183b7825aa7f1e78344f55c42c455f6816026
Yuri Jean Fabris <yuri.fabris@gmail.com>
2016-12-21 18:52:50 -0200

    Add lorem ipsum copyright home page text

--- app/imports/client/Pages/Home/index.js
+++ app/imports/client/Pages/Home/index.js
@@ -5,12 +5,66 @@
 
 // Modules
 import React from 'react'
+import styled from 'styled-components'
+import { grey700 } from 'material-ui/styles/colors'
+
+//Styled Components
+const P = styled.p`
+	text-align: justify;
+	color: ${grey700};
+	font-size: 15px;
+	line-height: 25px;
+`
 
 export const Home = (props) => (
-  <div>
-    <p>Home page</p>
+  <div
+  	style={{
+  		padding: '30px 150px',
+  		display: 'flex',
+  	}}
+  >
+    <P
+    	style={{
+    		paddingRight: 25,
+    	}}
+    >
+    	Lorem ipsum Fugiat dolor laboris commodo Ut dolore cupidatat ut in ut Duis.
+    	Lorem ipsum Aliqua consectetur elit ut elit consequat.
+    	Lorem ipsum Quis velit nulla eiusmod enim aliqua occaecat nostrud nostrud.
+    	Lorem ipsum Irure et ullamco nisi laboris labore do commodo in sint.
+    	Lorem ipsum Quis dolor dolore voluptate in in pariatur dolor in tempor labore in.
+    	Lorem ipsum Laboris sit consequat do in non ut nisi et ut proident.
+    	Lorem ipsum Consequat quis laborum non culpa exercitation ad dolor adipisicing sed incididunt labore velit.
+    	Lorem ipsum Aute reprehenderit ut pariatur deserunt proident enim proident.
+    	Lorem ipsum Dolore cillum eu incididunt ullamco ad reprehenderit dolore minim anim irure culpa.
+    	Lorem ipsum Enim cupidatat anim proident dolor ad ullamco minim adipisicing ea sit.
+    	Lorem ipsum Duis mollit commodo exercitation et magna eu veniam.
+    	Lorem ipsum Adipisicing ex laboris cillum sed do nisi nulla ut consectetur consectetur.
+    	Lorem ipsum Labore aliquip culpa in Duis sunt Duis sunt dolore.
+    	Lorem ipsum Dolor consectetur incididunt exercitation deserunt incididunt minim.
+    </P>
+
+    <P
+    	style={{
+    		paddingLeft: 25,
+    	}}
+    >
+    	Lorem ipsum Do do fugiat sed laboris aliquip voluptate dolor.
+    	Lorem ipsum Culpa do amet aliqua do non in nulla Ut Ut exercitation.
+    	Lorem ipsum Deserunt eu irure dolor non laboris voluptate adipisicing in dolor.
+    	Lorem ipsum Anim magna reprehenderit dolor cupidatat anim quis.
+    	Lorem ipsum Commodo in consectetur Duis dolore sed quis reprehenderit ea fugiat aliquip adipisicing dolor.
+    	Lorem ipsum Ea dolor magna in tempor tempor in mollit in ullamco.
+    	Lorem ipsum Deserunt magna tempor non aliquip pariatur.
+    	Lorem ipsum Nulla ea laborum amet exercitation proident voluptate enim ut deserunt.
+    	Lorem ipsum Deserunt qui mollit irure culpa pariatur ad minim in aute velit id.
+    	Lorem ipsum Mollit dolor incididunt proident dolor aliquip aliquip aliqua.
+    	Lorem ipsum Quis sed adipisicing laboris ex deserunt cupidatat Ut.
+    	Lorem ipsum Mollit commodo nostrud sunt deserunt irure pariatur veniam Duis ex.
+    	Lorem ipsum In officia sit amet in dolor laborum qui.
+    	Lorem ipsum Sint irure dolore dolor enim voluptate exercitation incididunt esse deserunt ad esse.
+    </P>
   </div>
 )
 
 export default Home
-
