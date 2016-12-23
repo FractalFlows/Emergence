28490ef3799dfc7afad60a834348f8a83b7dc783
Luiz Moratelli <la.moratelli@gmail.com>
2016-12-23 17:34:49 -0200

    Add close button

--- app/imports/client/Components/Modal/index.js
+++ app/imports/client/Components/Modal/index.js
@@ -3,27 +3,62 @@
  * @flow
  */
 
+//Modules
 import React from 'react'
+import ClearIcon from 'material-ui/svg-icons/content/clear'
+import {
+  white,
+	grey600,
+} from 'material-ui/styles/colors'
 
 export default class Modal extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isOpen: false,//this.props.isOpen
+    }
+  }
+  update(e) {
+    this.setState({
+			isOpen: this.props.isOpen,
+		})
+  }
   propTypes: {
     isOpen: React.PropTypes.boolean.isRequired,
   }
+  _closeModal() {
+    this.setState({
+      isOpen: false,
+    })
+  }
   render() {
+    let isOpen = this.state.isOpen
     return (
       <div
         style={{
-          backgroundColor: 'rgba(255, 255, 255, 0.6)',
-          display: this.props.isOpen ? 'block' : 'none',
+          backgroundColor: 'white',
+          display: isOpen ? 'block' : 'none',
           height: '100vh',
           left: 0,
-          postion: 'fixed',
+          opacity: 0.95,
+          position: 'fixed',
           top: 0,
+          transition: '0.5s',
           width: '100vw',
           zIndex: 1000,
         }}
       >
-        Testando
+      <ClearIcon
+        color={grey600}
+        style={{
+          height: 30,
+          position: 'absolute',
+          right: 20,
+          top: 10,
+          width: 30,
+        }}
+        onClick={this._closeModal.bind(this)}
+      />
       </div>
     )
   }
