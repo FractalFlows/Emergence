02ff90b7d5fd3c43d41c9dfbea3f977090ae5036
Gabriel Rubens <grsabreu@gmail.com>
2017-01-10 17:04:34 -0300

    Test knowledge bits ranking system

--- tests/articles/knowledge-bits.js
+++ tests/articles/knowledge-bits.js
@@ -3,10 +3,10 @@ import Articles from '../helpers/articles'
 import setFieldWithReduxForm from '../helpers/setFieldWithReduxForm'
 import sleep from '../helpers/sleep'
 
+const host = 'http://localhost:3000'
 describe('Articles', () => {
   describe('Knowledge Bits', () => {
-    describe('Upsert modal @watch', () => {
-      const host = 'http://localhost:3000'
+    describe('Upsert modal', () => {
       before(() => {
         server.call('dev/resetDatabase')
         server.call('dev/createArticles')
@@ -30,6 +30,85 @@ describe('Articles', () => {
         expect(updatedArticle.informations.length).to.equal(1)
       })
     })
+
+    describe('Ranking system @watch', () => {
+      before(() => {
+        server.call('dev/resetDatabase')
+        server.call('dev/createArticles')
+        browser.url(host)
+      })
+
+      it('user should be able to upvote a knowledge bit', () => {
+        const article = Articles('findOne', {'informations.upVotes': 0})
+
+        asAnUser()
+        navigateToArticle(article)
+        clickOnUpvoteBtnFor(article.informations[0])
+
+        // Hang a sec to wait ops complete
+        sleep(2000)
+
+        const updatedArticle = Articles('findOne', {_id: article._id})
+
+        expect(updatedArticle.informations[0].voters).to.be.an('array')
+        expect(updatedArticle.informations[0].voters).to.have.property('length', 1)
+        expect(updatedArticle.informations[0].voters[0].vote).to.equal(1)
+      })
+
+      it('user should be able to downvote a knowledge bit', () => {
+        const article = Articles('findOne', {'informations.upVotes': 1})
+
+        asAnUser()
+        navigateToArticle(article)
+        clickOnDownvoteBtnFor(article.informations[0])
+
+        // Hang a sec to wait ops complete
+        sleep(2000)
+
+        const updatedArticle = Articles('findOne', {_id: article._id})
+
+        expect(updatedArticle.informations[0].voters).to.be.an('array')
+        expect(updatedArticle.informations[0].voters).to.have.property('length', 1)
+        expect(updatedArticle.informations[0].voters[0].vote).to.equal(-1)
+      })
+
+      it('user should be able to undo downvote for a knowledge bit', () => {
+        const article = Articles('findOne', {'informations.downVotes': 1})
+
+        asAnUser()
+        navigateToArticle(article)
+
+        // As last test already downvoted clicking again will undo it
+        clickOnDownvoteBtnFor(article.informations[0])
+        // Hang a sec to wait ops complete
+        sleep(2000)
+
+        const updatedArticle = Articles('findOne', {_id: article._id})
+
+        expect(updatedArticle.informations[0].voters).to.be.an('array')
+        expect(updatedArticle.informations[0].voters).to.have.property('length', 0)
+      })
+
+      it('user should be able to undo a upvote for a knowledge bit', () => {
+        const article = Articles('findOne', {'informations.upVotes': 0})
+
+        asAnUser()
+        navigateToArticle(article)
+
+        clickOnUpvoteBtnFor(article.informations[0])
+        // Hang a sec to wait ops complete
+        sleep(2000)
+        // Click again to undo
+        clickOnUpvoteBtnFor(article.informations[0])
+        // Hang a sec to wait ops complete
+        sleep(2000)
+
+        const updatedArticle = Articles('findOne', {_id: article._id})
+
+        expect(updatedArticle.informations[0].voters).to.be.an('array')
+        expect(updatedArticle.informations[0].voters).to.have.property('length', 0)
+      })
+    })
   })
 })
 
@@ -39,7 +118,7 @@ function navigateToArticle(article){
 
 function clickOnAddKnowledgeProduct(){
   browser.waitForVisible('button[data-name="add-knowledge-btn"]')
-    browser.click('button[data-name="add-knowledge-btn"]')
+  browser.click('button[data-name="add-knowledge-btn"]')
 }
 
 function fillUpKnowledgeProductForm(){
@@ -55,4 +134,14 @@ function fillUpKnowledgeProductForm(){
     form: 'informationUpsert',
   })
   browser.setValue('[name="link"]', 'https://github.com/FractalFlows/Emergence')
+}
+
+function clickOnUpvoteBtnFor(knowledgeBit){
+  browser.waitForVisible(`[data-name="${knowledgeBit.link}-upvote-btn"]`)
+  browser.click(`[data-name="${knowledgeBit.link}-upvote-btn"]`)
+}
+
+function clickOnDownvoteBtnFor(knowledgeBit){
+  browser.waitForVisible(`[data-name="${knowledgeBit.link}-downvote-btn"]`)
+  browser.click(`[data-name="${knowledgeBit.link}-downvote-btn"]`)
 }
