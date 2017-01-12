45ad35893797429fa9b8cfafca75e28a1e6a81ae
Gabriel Rubens <grsabreu@gmail.com>
2017-01-12 16:38:06 -0300

    Add articles.hasUserReported check publication

--- app/imports/server/publications/article.js
+++ app/imports/server/publications/article.js
@@ -5,6 +5,21 @@ Meteor.publish('articles.bySlug', slug => (
   Articles.find({ slug }, {
     fields: {
       'informations.voters': 0,
+      unappropriatedContentReports: 0,
     },
   })
 ))
+
+// If the user has reported already it'll send only that very
+// entry added by the user so we can detect the state in the client
+Meteor.publish('articles.hasUserReported', function(slug){
+  return Articles.find({
+    slug,
+  }, {
+    fields: {
+      'unappropriatedContentReports': {
+        $elemMatch: { authorId: this.userId },
+      },
+    },
+  })
+})
