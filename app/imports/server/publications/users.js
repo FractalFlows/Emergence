4bac2624a4409cb9a5352a000f6dc4cfb3ad1f5d
Yuri Jean Fabris <yuri.fabris@gmail.com>
2017-01-18 17:17:57 -0500

    Add articles.all pub

--- app/imports/server/publications/users.js
+++ app/imports/server/publications/users.js
@@ -2,6 +2,9 @@ import { Meteor } from 'meteor/meteor'
 
 Meteor.publish('users.all', function(){
   if(this.userId){
-    return Meteor.users.find()
+    return Meteor.users.find({}, {
+      profile: 1,
+      emails: 1,
+    })
   }
 })
