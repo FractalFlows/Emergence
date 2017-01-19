8dabaad9ad0a6201d9f081f313310ba1c3787680
Gabriel Rubens <grsabreu@gmail.com>
2017-01-18 23:25:09 -0300

    Add router reducer

--- app/imports/client/reducers.js
+++ app/imports/client/reducers.js
@@ -2,7 +2,7 @@
 import { combineReducers } from 'redux'
 
 import { reducer as form } from 'redux-form'
-import { routerReducer as routing} from 'react-router-redux'
+import { routerReducer as routing } from 'react-router-redux'
 
 // Pages Reducers
 import user from './Pages/User/reducer'
