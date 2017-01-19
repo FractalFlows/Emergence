da79f005de4cb844012efbdb8345857eea322ae8
Gabriel Rubens <grsabreu@gmail.com>
2017-01-18 23:24:54 -0300

    Create fetchSelectedArticleLogic

--- app/imports/client/Pages/Search/logic.js
+++ app/imports/client/Pages/Search/logic.js
@@ -2,8 +2,13 @@ import { createLogic } from 'redux-logic'
 import {
   FETCH_SEARCH,
   LOAD_SEARCH_RESULTS,
+  FETCH_SELECTED_ARTICLE,
 } from './constants'
-import { loadSearchResults } from './actions'
+import router from 'react-router-redux'
+import {
+  loadSearchResults,
+  loadSelectedArticle,
+} from './actions'
 import { Meteor } from 'meteor/meteor'
 
 const fetchSearchLogic = createLogic({
@@ -22,10 +27,37 @@ const fetchSearchLogic = createLogic({
       dispatch(loadSearchResults({
         results,
       }))
+      done()
     })
   }
 })
 
+const fetchSelectedArticleLogic = createLogic({
+  type: FETCH_SELECTED_ARTICLE,
+  latest: true,
+  debounce: 200,
+  process({ action }, dispatch, done){
+    const { DOI, source } = action.payload
+
+    Meteor.call('articles/insertFromDOI', {
+      DOI,
+      source,
+    }, (error, slug) => {
+      if(error){
+        console.warn(error)
+        return
+      }
+
+      dispatch(loadSelectedArticle())
+      dispatch(router.push({
+        pathname: `/article/${slug}`,
+      }))
+      done()
+    })
+  },
+})
+
 export default [
   fetchSearchLogic,
+  fetchSelectedArticleLogic,
 ]
