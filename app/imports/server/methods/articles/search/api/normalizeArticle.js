4a51de232b112273ee95efc77b0399a0383ce56a
Gabriel Rubens <grsabreu@gmail.com>
2017-01-19 13:44:13 -0300

    Put normalizers outside function in normalizeArticle

--- app/imports/server/methods/articles/search/api/normalizeArticle.js
+++ app/imports/server/methods/articles/search/api/normalizeArticle.js
@@ -1,27 +1,27 @@
 import striptags from 'striptags'
 
+const normalizers = {
+  CrossRef(article){
+    return {
+      title: article['title'][0] ||article['container-title'][0] || article['original-title'][0],
+      abstract: striptags(article.abstract),
+      authors: article.author.map(author => author.literal || `${author.given} ${author.family}`),
+      DOI: article.DOI,
+      source,
+    }
+  },
+
+  DataCite({ attributes: article }){
+    return {
+      title: article.title,
+      abstract: striptags(article.description),
+      authors: article.author.map(author => author.literal || `${author.given} ${author.family}`),
+      DOI: article.doi,
+      source,
+    }
+  },
+}
+
 export default function normalizeArticle({ source, article }){
-  const normalizers = {
-    CrossRef(article){
-      return {
-        title: article['title'][0] ||article['container-title'][0] || article['original-title'][0],
-        abstract: striptags(article.abstract),
-        authors: article.author.map(author => author.literal || `${author.given} ${author.family}`),
-        DOI: article.DOI,
-        source,
-      }
-    },
-
-    DataCite({ attributes: article }){
-      return {
-        title: article.title,
-        abstract: striptags(article.description),
-        authors: article.author.map(author => author.literal || `${author.given} ${author.family}`),
-        DOI: article.doi,
-        source,
-      }
-    },
-  }
-
   return normalizers[source](article)
 }
