cbd4f36131ca1f57e65ba68b56c3be2e164b0731
Gabriel Rubens <grsabreu@gmail.com>
2017-01-20 21:48:39 -0300

    Create helper indexArticleToElastic

---
+++ app/imports/server/helpers/articles/indexToElastic.js
@@ -0,0 +1,25 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { ELASTIC_SEARCH_INDEX, ELASTIC_SEARCH_TYPE } from '/imports/both/collections/articles'
+import elasticSearch from '/imports/server/helpers/elasticSearch'
+
+export default function indexArticleToElastic({ authors, title, abstract, DOI }){
+  new SimpleSchema({
+    authors: { type: [String] },
+    title: { type: String },
+    abstract: { type: String },
+    DOI: { type: String },
+  }).validate({ authors, title, abstract, DOI })
+
+  elasticSearch.index({
+    index: ELASTIC_SEARCH_INDEX,
+    type: ELASTIC_SEARCH_TYPE,
+    body: {
+      authors,
+      title,
+      abstract,
+      DOI,
+    },
+  })
+
+  return true
+}
