5e925c81d78a90e2beaccbd50fbdae46be147def
Yuri Jean Fabris <yuri.fabris@gmail.com>
2017-01-24 04:40:26 -0500

    Add login tests

---
+++ tests/login/login.js
@@ -0,0 +1,41 @@
+const host = 'http://localhost:3000'
+
+describe('Login', () => {
+  before(() => {
+    server.call('dev/resetDatabase')
+    browser.url(host)
+  })
+
+  it('when clicking on "login" should open a modal', () => {
+    browser.click('a[data-name=header-login-btn]')
+    browser.waitForVisible('div.modal')
+  })
+
+  it('user should be able to login using email', () => {
+    fillupLoginForm({
+      email: 'johndoe@gmail.com',
+    })
+    hitLoginFormSubmit()
+    checkIfUserIsLoggedIn()
+
+    function fillupLoginForm({ email }) {
+      browser.waitForVisible('input[name=email]')
+      browser.setValue('input[name=email]', email)
+    }
+
+    function hitLoginFormSubmit(){
+      browser.click('form button[type=submit]')
+    }
+
+    function checkIfUserIsLoggedIn(){
+      browser.waitUntil(() => {
+        const userIsLoggedIn = browser
+          .execute(() => {
+            return !!Meteor.user()
+          }).value
+
+        return userIsLoggedIn
+      }, 5000, 'expected Meteor.user() to be not null')
+    }
+  })
+})
