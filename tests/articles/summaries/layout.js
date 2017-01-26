ee76ea996a4531c0534a3e564e7e43f87afd5674
Yuri Jean Fabris <yuri.fabris@gmail.com>
2017-01-26 14:12:26 -0500

    Add article summaries layout test

--- tests/articles/summaries/layout.js
+++ tests/articles/summaries/layout.js
@@ -1,4 +1,7 @@
 import {
+  Articles,
+  asAnUser,
+  navigateToArticle,
 } from '../../helpers'
 
 const host = 'http://localhost:3000'
@@ -8,12 +11,18 @@ describe('Articles', () => {
     describe('Upsert modal', () => {
       before(() => {
         server.call('dev/resetDatabase')
+        server.call('dev/createArticles')
         browser.url(host)
       })
 
       it('should be visible button "create a new summary" when none exists', () => {
+        //This article doesn't contain any summary
+        const article = Articles('findOne', {DOI: '10.1109/FIE.2000.896576'})
 
+        asAnUser()
+        navigateToArticle(host, article)
 
+        browser.getText('button=Create a new summary')
       })
 
       it('should have expandable items', () => {
