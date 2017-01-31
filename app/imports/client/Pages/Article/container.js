cb48c5a97f3a56f59766568dc699ac2296b63eaf
Gabriel Rubens <grsabreu@gmail.com>
2017-01-30 22:30:55 -0300

    Update Article container for redux

--- app/imports/client/Pages/Article/container.js
+++ app/imports/client/Pages/Article/container.js
@@ -4,10 +4,20 @@
  */
 
 import composeWithTracker from 'compose-with-tracker'
+import { compose } from 'recompose'
 import { Meteor } from 'meteor/meteor'
 import Articles from '/imports/both/collections/articles'
+import { connect } from 'react-redux'
+import * as actionsToProps from './actions'
 
-export default composeWithTracker((props, onData) => {
+export const redux = connect(
+  ({ article }) => ({
+    articleState: article,
+  }),
+  actionsToProps
+)
+
+export const meteor = composeWithTracker((props, onData) => {
   const slug = props.params.slug
   const handle = Meteor.subscribe('articles.bySlug', slug)
 
@@ -27,3 +37,8 @@ export default composeWithTracker((props, onData) => {
   propsToWatch: 'params',
   pure: true,
 })
+
+export default compose(
+  meteor,
+  redux
+)
