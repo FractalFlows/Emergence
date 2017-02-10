32d8d851cdfde1f2f146da0166b781e919d5d1ca
Gabriel Rubens <grsabreu@gmail.com>
2017-02-10 15:51:07 -0300

    Update elasticSearch service init

--- app/imports/server/helpers/elasticSearch.js
+++ app/imports/server/helpers/elasticSearch.js
@@ -1,10 +1,10 @@
 import elasticSearch from 'elasticsearch'
 import { Meteor } from 'meteor/meteor'
 
-const { elasticSearch: { user, password } } = Meteor.settings
+const { elasticSearch: { url, user, password } } = Meteor.settings
 
 const client = new elasticSearch.Client({
-  host: `https://${user}:${password}@e39fe5ed9579d46b459db0fec2cd7c47.us-east-1.aws.found.io:9243`,
+  host: `https://${user}:${password}@${url}`,
   log: 'trace',
 })
 
