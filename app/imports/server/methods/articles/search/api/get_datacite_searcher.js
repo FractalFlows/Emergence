508da985ba58b47aee03d3b344fdf85fc6c0a367
Gabriel Rubens <grsabreu@gmail.com>
2017-02-13 16:52:48 -0300

    Prevent crashing on DataCite 500 answer

--- app/imports/server/methods/articles/search/api/get_datacite_searcher.js
+++ app/imports/server/methods/articles/search/api/get_datacite_searcher.js
@@ -5,6 +5,11 @@ export default function getDataCiteSearcher(){
     url: 'https://api.datacite.org/works?rows=10&include=description&query',
     queryHandler: ({ search, url }) => `${url}=${search.replace(/\s+/, '+').toLowerCase()}`,
     responseHandler(data){
+      if(!data.data){
+        console.log('Error while fetching query from DataCite', data)
+        return []
+      }
+
       return data.data.map(article => normalizeArticle({
         article,
         source: 'DataCite',
