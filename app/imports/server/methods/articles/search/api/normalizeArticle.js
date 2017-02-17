15ba4585879ed9aa793915d4d9eb56374d7917d3
Gabriel Rubens <grsabreu@gmail.com>
2017-02-16 22:32:32 -0300

    Update article normalizers to handle unescaped html entities

--- app/imports/server/methods/articles/search/api/normalizeArticle.js
+++ app/imports/server/methods/articles/search/api/normalizeArticle.js
@@ -1,11 +1,12 @@
 import striptags from 'striptags'
+import { escape, map } from 'lodash/fp'
 
 const normalizers = {
   CrossRef(article, source){
     return {
-      title: article['title'][0] ||article['container-title'][0] || article['original-title'][0],
-      abstract: striptags(article.abstract),
-      authors: article.author.map(author => author.literal || `${author.given} ${author.family}`),
+      title: article['title'][0] || article['container-title'][0] || article['original-title'][0],
+      abstract: normalizeAbstract(article.abstract),
+      authors: normalizeAuthors(article.author),
       DOI: article.DOI,
       source,
     }
@@ -14,12 +15,20 @@ const normalizers = {
   DataCite({ attributes: article }, source){
     return {
       title: article.title,
-      abstract: striptags(article.description),
-      authors: article.author.map(author => author.literal || `${author.given} ${author.family}`),
+      abstract: normalizeAbstract(article.description),
+      authors: normalizeAuthors(article.author),
       DOI: article.doi,
       source,
     }
   },
+}
+
+function normalizeAuthors(authors){
+  return map(author => author.literal || `${author.given} ${author.family}`, authors)
+}
+
+function normalizeAbstract(abstract){
+  return striptags(escape(abstract))
 }
 
 export default function normalizeArticle({ source, article }){
