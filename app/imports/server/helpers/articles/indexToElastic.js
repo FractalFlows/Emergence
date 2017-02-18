7b7a1d51df6765051f727b7f391b648acc4d41c9
Gabriel Rubens <grsabreu@gmail.com>
2017-02-17 22:22:38 -0300

    Defer elastic indexing execution in indexToElastic with Meteor

--- app/imports/server/helpers/articles/indexToElastic.js
+++ app/imports/server/helpers/articles/indexToElastic.js
@@ -1,4 +1,5 @@
 import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { Meteor } from 'meteor/meteor'
 import { ELASTIC_SEARCH_INDEX, ELASTIC_SEARCH_TYPE } from '/imports/both/collections/articles'
 import elasticSearch from '/imports/server/helpers/elasticSearch'
 
@@ -10,15 +11,17 @@ export default function indexArticleToElastic({ authors, title, abstract, DOI })
     DOI: { type: String },
   }).validate({ authors, title, abstract, DOI })
 
-  elasticSearch.index({
-    index: ELASTIC_SEARCH_INDEX,
-    type: ELASTIC_SEARCH_TYPE,
-    body: {
-      authors,
-      title,
-      abstract,
-      DOI,
-    },
+  Meteor.defer(() => {
+    elasticSearch.index({
+      index: ELASTIC_SEARCH_INDEX,
+      type: ELASTIC_SEARCH_TYPE,
+      body: {
+        authors,
+        title,
+        abstract,
+        DOI,
+      },
+    })
   })
 
   return true
